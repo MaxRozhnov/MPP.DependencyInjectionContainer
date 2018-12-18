720e7c3002726630cbc582fd1e4d93307d75b83e
max <skymax1998@gmail.com>
2018-12-18 10:08:21 +0300

    Was proposed an idea about lifecycle enumeration. Would be stupid not to implement it

--- DependencyInjectionContainerLib/DependencyInjectionConfiguration.cs
+++ DependencyInjectionContainerLib/DependencyInjectionConfiguration.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Collections.Generic;
+using DependencyInjectionContainerLib.Utils;
+
 namespace DependencyInjectionContainerLib
 {
     public class DependencyInjectionConfiguration
     {
-        
+        private readonly Dictionary<Type, List<RegisteredTypeInfo>> _registeredTypes;
+
+        public DependencyInjectionConfiguration()
+        {
+            _registeredTypes = new Dictionary<Type, List<RegisteredTypeInfo>>();
+        }
+
+
+
+
+
+
+        private void RegisterNewPair(Type _interface, Type _implementation, LifecycleType _lifecycleType = LifecycleType.InstancePerDependency)
+        {
+            if (!_implementation.IsInterface && 
+                !_implementation.IsAbstract &&
+                _interface.IsAssignableFrom(_implementation))
+            {
+                var registeredType = new RegisteredTypeInfo(_interface, _implementation, _lifecycleType);
+            }
+        }
     }
 }
