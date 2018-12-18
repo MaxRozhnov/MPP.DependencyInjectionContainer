37018ee75037032c6f95398c1dc739637dd6f28d
max <skymax1998@gmail.com>
2018-12-18 11:18:53 +0300

    Created couple method templates for future implementation of DIC logic

--- DependencyInjectionContainerLib/DependencyInjectionContainer.cs
+++ DependencyInjectionContainerLib/DependencyInjectionContainer.cs
@@ -1,7 +1,33 @@
-﻿namespace DependencyInjectionContainerLib
+﻿using System;
+using DependencyInjectionContainerLib.Utils;
+
+namespace DependencyInjectionContainerLib
 {
     public class DependencyInjectionContainer
     {
+        private readonly DependencyInjectionConfiguration _configuration;
         
+        public DependencyInjectionContainer(DependencyInjectionConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        private object GetInstance(RegisteredTypeInfo registeredType)
+        {
+            if (registeredType.Lifecycle == LifecycleType.Singleton)
+            {
+                //TODO return a singleton
+            }
+            else
+            {
+                //TODO return a new instance
+            }
+        }
+
+        private object Instantiate(Type type)
+        {
+            
+        }
+
     }
 }
